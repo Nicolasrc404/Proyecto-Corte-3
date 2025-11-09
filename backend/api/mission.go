9bb27e798f69f96addffc7c0aba4dfad66215af7
Nicolas R <rubianon10@gmail.com>
2025-11-08 22:23:20 -0500

    Version 4.2

--- backend/api/mission.go
+++ backend/api/mission.go
@@ -4,14 +4,7 @@ type MissionRequestDto struct {
 	Title       string `json:"title"`
 	Description string `json:"description"`
 	Difficulty  string `json:"difficulty"`
-	AssignedTo  uint   `json:"assigned_to"` // AlchemistID
-}
-
-type MissionEditRequestDto struct {
-	Title       *string `json:"title,omitempty"`
-	Description *string `json:"description,omitempty"`
-	Difficulty  *string `json:"difficulty,omitempty"`
-	Status      *string `json:"status,omitempty"`
+	AssignedTo  uint   `json:"assigned_to"`
 }
 
 type MissionResponseDto struct {
